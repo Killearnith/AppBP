0cc6cd4fe3635dcc2681e85c0022ce532407cc92
Diego <diegamer10@gmail.com>
2022-02-20 20:24:27 +0100

    v2.1 Falta arreglar front de activity_telefono.xml

--- app/src/main/java/com/example/appbp/MainActivity.java
+++ app/src/main/java/com/example/appbp/MainActivity.java
@@ -3,13 +3,22 @@ package com.example.appbp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "HASH" ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTheme(R.style.Theme_AppBP);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_telefono);
+
+        /*
+        // Inside Main Activity
+        Log.d(TAG, "HashKey: " + appSignatureHashHelper.getAppSignatures().get(0));
+
+         */
     }
 }
